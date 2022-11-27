28b9209b1d9192ac0a3bb3824b10bda9d6a65910
Mugabodenys <mugabodan2020@gmail.com>
2022-11-27 23:24:34 +0200

    fixed dev

---
+++ pages/about/index.js
@@ -0,0 +1,9 @@
+const services = () => {
+    return ( 
+        <div>
+            <h2 className="text-3xl font-semibold">This is the About Page</h2>
+        </div>
+     );
+}
+ 
+export default services;
---
+++ pages/projects/index.js
@@ -0,0 +1,9 @@
+const services = () => {
+    return ( 
+        <div>
+            <h2>These are Our Projects</h2>
+        </div>
+     );
+}
+ 
+export default services;
